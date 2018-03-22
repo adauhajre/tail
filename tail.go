e11c0be2f8459f9e0691f95121a5c5acb4564719
Abraham Dauhajre <abraham@savioke.com>
2018-03-22 18:48:56 -0400

    removing all deps and vendoring.

--- tail.go
+++ tail.go
@@ -15,9 +15,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hpcloud/tail/ratelimiter"
-	"github.com/hpcloud/tail/util"
-	"github.com/hpcloud/tail/watch"
+	"github.com/adauhajre/tail/ratelimiter"
+	"github.com/adauhajre/tail/util"
+	"github.com/adauhajre/tail/watch"
 	"gopkg.in/tomb.v1"
 )
 
